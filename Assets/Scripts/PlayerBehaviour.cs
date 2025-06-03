3b9c033a1dd6da625665ac1263f0e693cabfdab1
tathetruong123 <truongttps40870@gmail.com>
2025-06-03 11:49:09 +0700

    map 1

--- Assets/Scripts/PlayerBehaviour.cs
+++ Assets/Scripts/PlayerBehaviour.cs
@@ -11,6 +11,13 @@ public class PlayerBehaviour : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
-        
+        if (SceneManager.GetActiveScene().name == "Map 1")
+        {
+            transform.Translate(Vector2.right * moveSpeed * Time.deltaTime);
+            if (Input.GetMouseButtonDown(0)) 
+            {
+                rb.AddForce(Vector2.up * jumpForce, ForceMode2D.Impulse);
+            }
+        }
     }
 }
